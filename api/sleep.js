eda004d839bac332919db0bf014b38761fc1d22c
mikus <127603974+mikus-furry@users.noreply.github.com>
2024-11-07 03:05:56 +0100

    Create sleep.js

---
+++ api/sleep.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async (req, res) => {
+    const baseUrl = "https://raw.githubusercontent.com/mikus-furry/api.github.oi/main/assets/img/";
+
+    const folderPath = path.join(process.cwd(), 'assets/img/sleep/');
+
+    const results = [];
+
+    const walkDirectory = (dirPath) => {
+        const files = fs.readdirSync(dirPath);
+        files.forEach((file) => {
+            const fullPath = path.join(dirPath, file);
+            const stat = fs.statSync(fullPath);
+
+            if (stat.isDirectory()) {
+                walkDirectory(fullPath);
+            } else if (file.match(/\.(png|jpg|jpeg|gif)$/)) {
+                const imageUrl = baseUrl + path.relative(folderPath, fullPath).replace(/\\/g, "/");
+                results.push({ "url": imageUrl });
+            }
+        });
+    };
+
+    walkDirectory(folderPath);
+
+    res.status(200).json({ results });
+};
